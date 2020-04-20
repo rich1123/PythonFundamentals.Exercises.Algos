0653a706c08a3ed7339eb20b88a30ac7bdeab8ff
Rich Maiale <richm1123@gmail.com>
2020-04-20 14:33:50 -0400

    code compiles, 1st test passes, 2nd test for type not working

--- search.py
+++ search.py
@@ -2,23 +2,24 @@ import math
 
 
 def binary_search(list_in: list, item: int):
-    l_ordered = sorted(list_in)
-    middle = math.ceil(len(l_ordered) / 2)
-    if item > l_ordered[-1]:
-        print(f'{item} is not in the list')
-    elif item < l_ordered[0]:
-        print(f'{item} is not in the list')
-    elif item == middle:
-        print(f'{item} is in list, {l_ordered.index(item)} is the index')
-    # elif item != l_length
-    elif item > list_in[middle]:
-        binary_search(list_in[middle:-1], item)
-    elif item < list_in[middle]:
-        binary_search(list_in[0:middle], item)
+    if item in list_in:
 
+        l_ordered = sorted(list_in)
+        middle = math.ceil(len(l_ordered) / 2)
+        # print(middle)
 
+        if item == middle:
+            print(f'{item} is in list, {l_ordered.index(item)} is the index')
+            return l_ordered.index(item)
+        elif item < l_ordered[middle]:
+            return binary_search(list_in[0:middle], item)
+        elif item > l_ordered[middle]:
+            return binary_search(list_in[middle:-1], item)
+    else:
+        return None
 
 if __name__ == "__main__":
-    binary_search([1, 2, 3, 4, 5], 1)
-    binary_search([1, 2, 3, 4, 5, 6, 7, 8, 9], 12)
-    binary_search([1, 2, 3, 4, 5, 6, 7, ], -1)
+    binary_search([1, 2, 3, 4, 5], 3)
+    binary_search([1, 2, 3, 4, 5], 2)
+    binary_search([1, 2, 3, 4, 5], 3)
+    binary_search([1, 2, 3, 4, 5], -1)
